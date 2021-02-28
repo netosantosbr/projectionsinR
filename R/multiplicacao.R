e68db605a21b727f4fcfcb974d5754ce094dcb13
José Alfredo <netoalfredo9@gmail.com>
2021-02-28 10:28:21 -0300

    first commit

---
+++ R/multiplicacao.R
@@ -0,0 +1,14 @@
+#' Função Multiplicadora
+#'
+#' Esta função serve para multiplicar dois números.
+#'
+#' @param a Um número.
+#' @param b Outro número.
+#'
+#' @examples
+#' multiplicar(1, 2)
+#'
+#' @export
+multiplicar <- function(a, b){
+  a * b
+}
